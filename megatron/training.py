653f2f5922e1672a29609a77b7d1093d8a54cdc2
shjwudp <shjwudp@gmail.com>
2021-09-30 14:32:55 +0800

    ..

--- megatron/training.py
+++ megatron/training.py
@@ -250,6 +250,7 @@ def get_model(model_provider_func):
         model = [
             model_module.with_bagua(
                 algorithm=gradient_allreduce.GradientAllReduceAlgorithm(),
+                optimizers=[],
                 process_group=bagua.communication.from_torch_group(
                     mpu.get_data_parallel_group())
             ) for model_module in model]
