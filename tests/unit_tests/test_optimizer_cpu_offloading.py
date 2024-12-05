d9d4607e2f9fd0fa57e5a72bcd56b27b14ebc705
lostkevin <lostkevin666@gmail.com>
2024-12-05 08:58:04 +0000

    add unittest and fix bug

--- tests/unit_tests/test_optimizer_cpu_offloading.py
+++ tests/unit_tests/test_optimizer_cpu_offloading.py
@@ -7,8 +7,6 @@ from torch.optim import SGD, Adam
 from transformer_engine.pytorch.optimizers import FusedAdam, FusedSGD
 
 from megatron.core.optimizer.cpu_offloading import HybridDeviceOptimizer
-from megatron.core.optimizer import _get_param_groups
-
 
 class Net(nn.Module):
     def __init__(self):
@@ -29,12 +27,24 @@ class Net(nn.Module):
         x = self.fc3(x)
         return x
 
-
+@pytest.mark.parametrize('n_steps', [1, 10])
+@pytest.mark.parametrize('overlap_strategy', ['no_overlap', 'streams', 'overlap'])
+@pytest.mark.parametrize('offload_fraction', [0, 0.5, 1.0])
+@pytest.mark.parametrize('optimizer', ['sgd', 'adam'])
 @pytest.mark.parametrize('with_param_groups', [False, True])
-def test_multi_device_hybrid_optimizer(with_param_groups):
-    net = Net().cuda()
-
-    params = list(net.parameters())
+def test_multi_device_hybrid_optimizer(
+    with_param_groups, 
+    optimizer,
+    offload_fraction,
+    overlap_strategy,
+    n_steps
+):
+    net1 = Net().cuda()
+    net2 = Net().cuda()
+    net2.load_state_dict(net1.state_dict())
+    base_lr = 1e-3
+    params = list(net1.parameters())
+    ref_params = list(net2.parameters())
     if with_param_groups:
         param_groups = [
             {"params": params[: len(params) // 2],
@@ -46,25 +56,80 @@ def test_multi_device_hybrid_optimizer(with_param_groups):
             "lr_mult": 0.2,},
         ]
         params = param_groups
+        ref_param_groups = [
+            {"params": ref_params[: len(ref_params) // 2],
+            "wd_mult": 0.1,
+            "lr_mult": 0.1,
+            },
+            {"params": ref_params[len(ref_params) // 2 :],
+            "wd_mult": 0.2,
+            "lr_mult": 0.2,},
+        ] 
+        ref_params = ref_param_groups    
 
+    if optimizer =='adam':
+        cls_kwargs = dict(cpu_optimizer_cls=Adam, gpu_optimizer_cls=FusedAdam)
+    else:
+        cls_kwargs = dict(cpu_optimizer_cls=SGD, gpu_optimizer_cls=FusedSGD)
+
+    if overlap_strategy == 'no_overlap':
+        overlap_kwargs = dict(overlap=False, multi_streams=False)
+    elif overlap_strategy == 'streams':
+        overlap_kwargs = dict(overlap=False, multi_streams=True)
+    else:
+        overlap_kwargs = dict(overlap=True, multi_streams=True)
+       
     hdo = HybridDeviceOptimizer(
         params,
-        offload_fraction=0.5,
-        cpu_optimizer_cls=Adam,
-        gpu_optimizer_cls=FusedAdam,
-        lr=0.1,
+        offload_fraction=offload_fraction,
+        lr=base_lr,
+        **cls_kwargs,
+        **overlap_kwargs
+    )
+
+    ref_optimizer = cls_kwargs['gpu_optimizer_cls'](
+        ref_params,
+        lr=base_lr,
     )
 
     # 1. run step on optimizer, make sure there is state generated
     assert len(hdo.state_dict()["state"]) == 0  # state is empty
     input = torch.randn(1, 3, 32, 32).cuda()
-    output = net(input)
+    output = net1(input)
     output.sum().backward()
     hdo.step()
-    assert len(hdo.state_dict()["state"]) != 0
+    output = net2(input)
+    output.sum().backward()
+    ref_optimizer.step()
+    # PyTorch SGD will not generate state
+    if optimizer != 'sgd' or offload_fraction < 1:
+        assert len(hdo.state_dict()["state"]) != 0
 
     # 2. check the state is on right device
-    first_param_id = hdo.state_dict()["param_groups"][0]["params"][0]
-    last_param_id = hdo.state_dict()["param_groups"][-1]["params"][-1]
-    assert not hdo.state_dict()["state"][first_param_id]["exp_avg"].is_cuda
-    assert hdo.state_dict()["state"][last_param_id]["exp_avg"].is_cuda
+    if optimizer == 'adam':
+        first_param_id = hdo.state_dict()["param_groups"][0]["params"][0]
+        last_param_id = hdo.state_dict()["param_groups"][-1]["params"][-1]
+        if offload_fraction > 0:
+            assert not hdo.state_dict()["state"][first_param_id]["exp_avg"].is_cuda
+        if offload_fraction < 1:
+            assert hdo.state_dict()["state"][last_param_id]["exp_avg"].is_cuda
+
+    for _ in range(1, n_steps):
+        input = torch.randn(1, 3, 32, 32).cuda()
+        output = net1(input)
+        output.sum().backward()
+        hdo.step()
+        output = net2(input)
+        output.sum().backward()
+        ref_optimizer.step()
+
+    # 3. check parameters allclose
+    params = net1.state_dict()
+    ref_params = net2.state_dict()
+    for k, v in params.items():
+        assert (v.isnan() == ref_params[k].isnan()).all()
+        torch.nan_to_num_(v, 0)
+        torch.nan_to_num_(ref_params[k], 0)
+        tol = 1e-6 if n_steps > 1 else 1e-8
+        assert torch.allclose(v, ref_params[k], atol=tol), f"Weight {k} value mismatch, max error: {(v - ref_params[k]).abs().max()}"
+    
