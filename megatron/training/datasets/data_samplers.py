e1d29ff52f661512f92197e3fb30bae1e58fb847
Deepak Narayanan <dnarayanan@nvidia.com>
2025-12-17 12:58:08 -0800

    Go back to using multiprocessing_context='fork' in DataLoader to fix various observed issues

--- megatron/training/datasets/data_samplers.py
+++ megatron/training/datasets/data_samplers.py
@@ -78,7 +78,6 @@ def build_pretraining_data_loader(dataset, consumed_samples):
         pin_memory=True,
         persistent_workers=True if args.num_workers > 0 else False,
         worker_init_fn=maybe_worker_init_fn,
-        multiprocessing_context='spawn',
     )
 
 
